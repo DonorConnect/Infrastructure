f929b8976e142fdb3c776a7425bda57ca53fdd01
sannidhij <sannidhi.j0101@gmail.com>
2012-10-24 16:05:34 +0530

    Sannidhi/Pooja Tech Stort #25: Modified go_setup file

--- go_setup.sh
+++ go_setup.sh
@@ -2,16 +2,16 @@
 
 yum install unzip -y
 yum install git -y
-service iptables stop
-rpm -ivh http://10.10.4.119/jdk-7u9-linux-x64.rpm 
-rpm -ivh http://10.10.4.119/go-server-12.3.2-15897.noarch.rpm
-sleep 30
-rpm -ivh http://10.10.4.119/go-agent-12.3.2-15897.noarch.rpm
-#while [true]
-#if(curl -I  --stderr /dev/null http://localhost:8153/go/home | head -1 | cut -d' ' -f2 != 200)
- # sleep 1000
-#else
- # break
-#done
-service go-agent start
 
+rpm -ivh http://10.10.4.119/jdk-7u9-linux-x64.rpm 
+
+#Install Go Server and its config
+mkdir /etc/go
+curl -o /etc/go/cruise-config.xml https://raw.github.com/DonorConnect/Infrastructure/master/cruise-config.xml
+rpm -ivh http://10.10.4.119/go-server-12.3.2-15897.noarch.rpm
+
+sleep 30 # wait for go server to start up.
+
+# Install GO Agent
+rpm -ivh http://10.10.4.119/go-agent-12.3.2-15897.noarch.rpm
+service go-agent restart
