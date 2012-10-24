1f0b1e786ec350665408c5d6d197545739c29c79
sannidhij <sannidhi.j0101@gmail.com>
2012-10-24 17:11:47 +0530

    Sannidhi/pooja Tech Story #25: Config file updated path

--- GO_server_setup_script/go_setup.sh
+++ GO_server_setup_script/go_setup.sh
@@ -7,7 +7,7 @@ rpm -ivh http://10.10.4.119/jdk-7u9-linux-x64.rpm
 
 #Install Go Server and its config
 mkdir /etc/go
-curl -o /etc/go/cruise-config.xml https://raw.github.com/DonorConnect/Infrastructure/master/cruise-config.xml
+curl -o /etc/go/cruise-config.xml https://raw.github.com/DonorConnect/Infrastructure/master/GO_server_setup_script/cruise-config.xml 
 rpm -ivh http://10.10.4.119/go-server-12.3.2-15897.noarch.rpm
 
 sleep 30 # wait for go server to start up.
