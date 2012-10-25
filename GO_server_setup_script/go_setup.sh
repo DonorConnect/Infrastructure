6037048a1effb6a211ca0a5d7da7acf15fe12b3a
Shari Cahill <scahill@thoughtworks.com>
2012-10-25 16:12:13 +0530

    Pooja/Shari tech story 27 : modified cruise-config and go_setup to fix go java version bug

--- GO_server_setup_script/go_setup.sh
+++ GO_server_setup_script/go_setup.sh
@@ -4,6 +4,12 @@ yum install unzip -y
 yum install git -y
 
 rpm -ivh http://10.10.4.119/jdk-7u9-linux-x64.rpm 
+wget http://10.10.4.119/jdk-6u34-linux-x64.bin
+chmod u+x jdk-6u34-linux-x64.bin
+exec jdk-6u34-linux-x64.bin
+mv jdk-6u34-linux-x64 /usr/java
+rm latest
+ln -s latest /usr/java/jdk1.6.0_09
 
 #Install Go Server and its config
 mkdir /etc/go
