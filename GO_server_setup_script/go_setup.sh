cf362d3e9707477381f6ad49168683761ef93347
Shari Cahill <scahill@thoughtworks.com>
2012-11-02 12:16:29 +0530

    Longkai/Shari : Remove ssh commands from go_setup script

--- GO_server_setup_script/go_setup.sh
+++ GO_server_setup_script/go_setup.sh
@@ -1,8 +1,14 @@
 #! /bin/bash
 
+#Install Unzip and Git
 yum install unzip -y
 yum install git -y
 
+#Install MySQL and start service
+yum install http://10.10.4.119/MySQL-server-5.5.28-1.linux2.6.x86_64.rpm -y
+service mysqld start
+
+#Install Java JDK v6 and v7
 rpm -ivh http://10.10.4.119/jdk-7u9-linux-x64.rpm 
 wget http://10.10.4.119/jdk-6u34-linux-x64.bin -O /usr/java/jdk-6u34-linux-x64.bin
 chmod u+x /usr/java/jdk-6u34-linux-x64.bin
@@ -11,20 +17,13 @@ cd /usr/java
 rm latest
 ln -s /usr/java/jdk1.6.0_34 latest
 
-#Create and copy Go-server's public key to Qa environment and deployment environment
-ssh-keygen -t dsa
-cd ~/.ssh
-chmod 755 .ssh
-scp ~/.ssh/id_dsa.pub roo@10.10.4.121:.ssh/authorized_keys
-scp ~/.ssh/id_dsa.pub roo@10.10.4.123:.ssh/authorized_keys
-
-#Install Go Server and its config
-mkdir /etc/go
+#Install GO-Server and its config
+mkdir -p /etc/go
 curl -o /etc/go/cruise-config.xml https://raw.github.com/DonorConnect/Infrastructure/master/GO_server_setup_script/cruise-config.xml 
 rpm -ivh http://10.10.4.119/go-server-12.3.2-15897.noarch.rpm
 
 sleep 30 # wait for go server to start up.
 
-# Install GO Agent
+# Install GO-Agent
 rpm -ivh http://10.10.4.119/go-agent-12.3.2-15897.noarch.rpm
 service go-agent restart
